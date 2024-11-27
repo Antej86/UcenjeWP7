a00e3fce4d9a355ce75e3740e6b622cceb5247be
Ante Janković <bhorv4t@gmail.com>
2024-11-27 19:17:38 +0100

    Update skriptawp7.sql

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -1,5 +1,13 @@
-﻿--create database edunovawp7;
---use edunovawp7;
+﻿use master;
+go
+drop database if exists edunovawp7;
+go
+
+create database edunovawp7;
+go
+
+use edunovawp7;
+go
 
 create table smjer(
 naziv varchar(50)
