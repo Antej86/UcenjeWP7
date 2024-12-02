4834f2500ec3d41ee16d291be7ed6c93ff63ef9b
Ante Janković <antejankovic86@gmail.com>
2024-12-02 19:47:45 +0100

    Create ispitnirokwp7.sql

---
+++ ispitnirokwp7.sql
@@ -0,0 +1,23 @@
+use master;
+go
+drop database if exists ispitnirokwp7;
+go
+create database ispitnirokwp7;
+go
+use ispitnirokwp7;
+go
+
+create table Ispitnirok(
+sifra int not null primary key identity(1,1),
+predmet varchar(50) not null,
+vrstaIspita varchar(50) not null,
+datum datetime,
+pristupio bit not null
+);
+
+create table Pristupnici(
+Ispitnirok datetime not null,
+student varchar(50) not null,
+brojbodova int not null,
+ocjena int not null
+);
