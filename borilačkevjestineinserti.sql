d7acedbb193b8cbf3b48f96d8c3473e98f19f52b
ante <antejankovic86@gmail.com>
2024-12-09 19:51:44 +0100

    Update borilačkevjestineinserti.sql

--- borilačkevjestineinserti.sql
+++ borilačkevjestineinserti.sql
@@ -17,7 +17,7 @@ insert into Klub
 ('Team K1', '1993-05-03', 'Kazuyoshi Ishii', 'Japan',4);
 
 Insert into Borac
-(ime,prezime,datum_rođenja,vještina,rekord,klub)
+(ime, prezime,datum_rođenja ,vještina ,rekord, klub) values
 
 INSERT INTO Borac (ime, prezime, datum_rođenja, vještina, rekord, klub)
 VALUES
@@ -29,3 +29,23 @@ VALUES
 ('Holly', 'Holm', '1981-10-17', 'Boxing, Kickboxing, Brazilian Jiu-Jitsu, MMA', '15–6',3),
 ('Giorgio', 'Petrosyan', '1985-06-10', 'Kickboxing, Muay Thai', '105–3–2',4),
 ('Andy', 'Souwer', '1981-08-20', 'Kickboxing, Muay Thai', '164–19–1',4);
+
+Insert into Trener (ime, prezime, klub, nacionalnost, iskustvo) values
+
+INSERT INTO Trainers (ime, prezime, klub, nacionalnost, iskustvo)
+VALUES
+-- American Top Team (ATT)
+('Mike', 'Brown', 1, 'USA', 'Former UFC fighter, Head Coach, 20+ years coaching experience'),
+('Krzysztof', 'Kryzstof', 1, 'Poland', 'Muay Thai and Boxing Coach, 15+ years experience'),
+
+-- Evolve MMA
+('Chatri', 'Sityodtong', 2, 'Thailand', 'Founder of Evolve MMA, Muay Thai and BJJ Coach, 20+ years experience'),
+('Sammy', 'Adebayo', 2, 'Singapore', 'BJJ and MMA coach, 10+ years experience'),
+
+-- Jackson Wink MMA Academy
+('Greg', 'Jackson', 3, 'USA', 'MMA coach, 25+ years experience, renowned strategist'),
+('Mike', 'Winkeljohn', 3, 'USA', 'MMA coach, 20+ years experience, striking specialist'),
+
+-- Team K-1
+('Kazuyoshi', 'Ishii', 4, 'Japan', 'Founder of K-1, kickboxing legend, 30+ years experience'),
+('Tatsuya', 'Koyama', 4, 'Japan', 'Head Muay Thai Coach, 20+ years experience');
